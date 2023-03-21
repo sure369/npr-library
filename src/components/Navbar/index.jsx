559c5afb9a57d30189111a84213ab378fab0c8cd
sure369 <sureshmahath@gmail.com>
2023-03-21 14:55:16 +0530

    final

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -38,7 +38,7 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static" sx={{ bgcolor: "white" }}>
+    <AppBar position="static" sx={{ bgcolor: "#FFFFD2" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
